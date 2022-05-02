b45519519c4d0e9a37e8768cfc5648bde9573cb4
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-02 16:05:18 +0200

    Stampa riuscita!

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 
+const app = new Vue({
+
+	el: '#app',
+
+	data: {
+		messaggio: 'Interessante la prima lezione col Career Service!'
+	}
+
+})
+
+
+
+
+
+
+
 
 /***************************
     VARIABILI GLOBALI
