adad559892059373406747b23dcfbda4caf88772
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-02 16:00:46 +0200

    Setup iniziale

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/*
+**Descrizione:**
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+**Bonus:**
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+
+/***************************
+    VARIABILI GLOBALI
+***************************/
+//
+
+
+/***************************
+    FUNZIONI DI UTILITà
+****************************/
+//
+
+
+/***************************
+    INIZIALIZZAZIONE
+****************************/
+//
+
