e71b5e93542475395ddbf7d53b0a6dfd4fc425c1
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-02 16:10:17 +0200

    Bonus completato!

--- js/script.js
+++ js/script.js
@@ -10,32 +10,9 @@ const app = new Vue({
 	el: '#app',
 
 	data: {
-		messaggio: 'Interessante la prima lezione col Career Service!'
+		messaggio: 'Interessante la prima lezione col Career Service!',
+		strauben: 'https://blog.giallozafferano.it/allacciateilgrembiule/wp-content/uploads/2020/08/strauben3.jpg',
 	}
 
 })
-
-
-
-
-
-
-
-
-/***************************
-    VARIABILI GLOBALI
-***************************/
-//
-
-
-/***************************
-    FUNZIONI DI UTILITà
-****************************/
-//
-
-
-/***************************
-    INIZIALIZZAZIONE
-****************************/
-//
 
